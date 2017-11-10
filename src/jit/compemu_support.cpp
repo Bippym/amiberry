965121f5b1331f03daade78ab0d10bf0a8f02ce4
Dimitris Panokostas <midwan@gmail.com>
2017-11-10 10:04:33 +0100

    Merged compemu_prefs inside compemu_support, removed compemu_prefs (not needed anymore)

--- src/jit/compemu_support.cpp
+++ src/jit/compemu_support.cpp
@@ -45,8 +45,6 @@
 #include "compemu.h"
 #include <SDL.h>
 
-#include "compemu_prefs.cpp"
-
 #define DEBUG 0
 
 #if DEBUG
@@ -644,6 +642,28 @@ STATIC_INLINE void alloc_blockinfos(void)
 
   	prepare_block(bi);
   }
+}
+
+bool check_prefs_changed_comp(bool checkonly)
+{
+	bool changed = 0;
+
+	if (currprefs.fpu_strict != changed_prefs.fpu_strict ||
+		currprefs.cachesize != changed_prefs.cachesize)
+		changed = 1;
+
+	if (checkonly)
+		return changed;
+
+	currprefs.fpu_strict = changed_prefs.fpu_strict;
+
+	if (currprefs.cachesize != changed_prefs.cachesize) {
+		currprefs.cachesize = changed_prefs.cachesize;
+		alloc_cache();
+		changed = 1;
+	}
+
+	return changed;
 }
 
 /********************************************************************
